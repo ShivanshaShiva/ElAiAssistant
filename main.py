7c51a4076590a78d66b13d5503cff11b73d1b35d
ShivanshaShiva <nkg7060@gmail.com>
2025-05-28 13:04:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy uic.button import Button
+from kivy uix.button import Button
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.config import Config
